e7dc87875079be2d03781e5a7ec83a9c0bdf45d9
Pavel T <paveltitushkin@gmail.com>
2023-08-23 21:45:52 +0300

    Настроен DbContext в InternalApi и PublicApi.

--- PetProject/PublicApi/Program.cs
+++ PetProject/PublicApi/Program.cs
@@ -2,16 +2,31 @@ using Fuse8_ByteMinds.SummerSchool.PublicApi;
 using Microsoft.AspNetCore;
 using Serilog;
 
-var webHost = WebHost
-    .CreateDefaultBuilder(args)
-    .UseStartup<Startup>()
-    .UseSerilog((context, config) =>
-    {
-        config.ReadFrom.Configuration(context.Configuration)
-        .MinimumLevel.Information()
-        .Enrich.WithMachineName()
-        .WriteTo.Console();
-    })
-    .Build();
+//var webHost = WebHost
+//    .CreateDefaultBuilder(args)
+//    .UseStartup<Startup>()
+//    .UseSerilog((context, config) =>
+//    {
+//        config.ReadFrom.Configuration(context.Configuration)
+//        .MinimumLevel.Information()
+//        .Enrich.WithMachineName()
+//        .WriteTo.Console();
+//    })
+//    .Build();
 
-await webHost.RunAsync();
+//await webHost.RunAsync();
+
+Host.CreateDefaultBuilder(args)
+  .ConfigureWebHostDefaults(
+    webBuilder =>
+    {
+        webBuilder.UseStartup<Startup>();
+        webBuilder.UseSerilog((context, config) =>
+                {
+                    config.ReadFrom.Configuration(context.Configuration)
+                    .MinimumLevel.Information()
+                    .Enrich.WithMachineName()
+                    .WriteTo.Console();
+                });
+    })
+      .Build().Run();
