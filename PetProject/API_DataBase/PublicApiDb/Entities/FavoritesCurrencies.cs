e4a04c32edbdb2f60ba5c99a51042d864cfaf522
Pavel T <paveltitushkin@gmail.com>
2023-08-24 14:09:12 +0300

    Добавлена и настроена таблица Избранных валют.

---
+++ PetProject/API_DataBase/PublicApiDb/Entities/FavoritesCurrencies.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStore.PublicApiDb.Entities
+{
+    public class FavoritesCurrencies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Currency { get; set; }
+        public string BaseCurrency { get; set; }
+    }
+}
---
+++ PetProject/API_DataBase/PublicApiDb/EntitiesConfig/FavoritesCurrenciesConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using DataStore.PublicApiDb.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataStore.PublicApiDb.EntitiesConfig
+{
+    public class FavoritesCurrenciesConfiguration : IEntityTypeConfiguration<FavoritesCurrencies>
+    {
+        public void Configure(EntityTypeBuilder<FavoritesCurrencies> builder)
+        {
+            builder.HasIndex(p => p.Name).IsUnique();
+            builder.HasIndex(p=> new {p.Currency, p.BaseCurrency}).IsUnique();
+        }
+    }
+}
